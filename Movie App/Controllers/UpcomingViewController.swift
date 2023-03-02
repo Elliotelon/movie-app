5d771bd7796e58707a846efce1adbb1b9ffa8d39
minkyu <elliotelon@gmail.com>
2023-03-02 17:28:06 +0900

    collectionView 추가

--- Movie App/Controllers/UpcomingViewController.swift
+++ Movie App/Controllers/UpcomingViewController.swift
@@ -12,7 +12,7 @@ class UpcomingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .green
+        view.backgroundColor = .systemBackground
     }
     
 }
--- Movie App/MainTabBarViewController.swift
+++ Movie App/MainTabBarViewController.swift
@@ -19,7 +19,20 @@ class MainTabBarViewController: UITabBarController {
         let downloadsVC = UINavigationController(rootViewController: DownloadsViewController())
         
         
+        homeVC.tabBarItem.image = UIImage(systemName: "house")
+        upcomingVC.tabBarItem.image = UIImage(systemName: "play.circle")
+        searchVC.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        downloadsVC.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
+        
+        homeVC.title = "홈"
+        upcomingVC.title = "개봉예정"
+        searchVC.title = "검색"
+        downloadsVC.title = "다운로드"
+        
+        tabBar.tintColor = .label
+        
         setViewControllers([homeVC, upcomingVC, searchVC, downloadsVC], animated: true)
+        //11:09
         
     }
 
