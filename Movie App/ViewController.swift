09600c7ba6eeabf7684409542432b728475b0700
minkyu <elliotelon@gmail.com>
2023-03-02 12:29:48 +0900

    Initial Commit

---
+++ Movie App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie App
+//
+//  Created by 김민규 on 2023/03/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
