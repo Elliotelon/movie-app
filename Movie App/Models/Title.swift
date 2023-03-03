b079b8896eed74c8089b5303b639de6782389172
minkyu <elliotelon@gmail.com>
2023-03-03 13:42:21 +0900

    개봉예정 UI

--- Movie App/Models/Title.swift
+++ Movie App/Models/Title.swift
@@ -15,7 +15,7 @@ struct Title: Codable {
     let id: Int
     let media_type: String?
     let original_name: String?
-    let origianl_title: String?
+    let original_title: String?
     let poster_path: String?
     let overview: String?
     let vote_count: Int
---
+++ Movie App/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Movie App
+//
+//  Created by 김민규 on 2023/03/03.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
